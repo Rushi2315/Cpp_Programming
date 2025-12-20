f533354ad5f2c2b2efe011f8a8d8f6d178164ed0
Rushi <deshmukhrushikesh51@gmail.com>
2025-12-20 16:48:11 +0530

    Assignment 1
    Assignment 1

---
+++ Assignment 1/Q1 Hello World C++ Example Program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;            ///Standard namespace declaration
+
+
+
+int main()                              ///main function
+{
+
+    cout<<"My First C++ Program";           /// standard output statement
+
+    getch();                                ///wait for output screen
+    return 0;                               /// main function return statement
+}
---
+++ Assignment 1/Q2 if statement example program in c++.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    //variable declaration
+    int a;
+
+    //get input value
+    cout<<"Enter the Number:";
+    cin>>a;
+
+
+    //if condition check
+    if()
+    {
+        //block for condition success
+        cout<<a<<"Is Greater than 10";
+    }
+
+    //wait for output screen
+    getch();
+    return 0
+}
