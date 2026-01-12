d3fe9a83c4df43aa1d908e516a41282ed7d56eb9
Rushi <deshmukhrushikesh51@gmail.com>
2026-01-12 19:31:36 +0530

    Assignment 1

---
+++ Assignment 1/This is first C++ Program.cpp
@@ -0,0 +1,13 @@
+/// This is first C++ Program
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   cout<<" Rushikesh Deshmukh" <<endl;
+   cout<<"A/P Tambave, Tal- Walwa, Dist- Sangli" <<endl;
+   cout<<"MCA Complete" <<endl;
+
+
+}
